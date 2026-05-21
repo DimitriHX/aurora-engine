5142d3a70fbb4a2cb9a4d31311b855b44d9b2976
dimitrihx <dimitrihx@gmail.com>
2026-05-21 17:19:56 -0600

    Agregue los estados de las teclas en Engine/Input

---
+++ src/Aurora.Engine/Input/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Input;
+namespace Aurora.Engine.Input;
+
+public class InputManager
+{
+    private KeyboardState _keyboardState;
+
+    public void Update()
+    {
+        _keyboardState = Keyboard.GetState();
+    }
+
+    public bool Up()
+        => _keyboardState.IsKeyDown(Keys.W);
+
+    public bool Down()
+        => _keyboardState.IsKeyDown(Keys.S);
+
+    public bool Left()
+        => _keyboardState.IsKeyDown(Keys.A);
+
+    public bool Right()
+        => _keyboardState.IsKeyDown(Keys.D);
+
+    public bool ExitRequested()
+        => _keyboardState.IsKeyDown(Keys.Escape);
+}
