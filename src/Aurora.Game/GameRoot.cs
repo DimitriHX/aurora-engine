e6e4a39884581f0ef8cea30177c6eb83ba302f76
dimitrihx <dimitrihx@gmail.com>
2026-05-21 17:51:39 -0600

    Modifique la clase de GameRoot por error de namespaces a nombre directo de libreria interna de Game

--- src/Aurora.Game/GameRoot.cs
+++ src/Aurora.Game/GameRoot.cs
@@ -3,15 +3,35 @@ using Microsoft.Xna.Framework.Graphics;
 using Aurora.Engine.Rendering;
 using Aurora.Engine.Input;
 using Aurora.Engine.Camera;
+using System;
 
 namespace Aurora.Game;
 
-public class GameRoot : Game
+public class GameRoot : Microsoft.Xna.Framework.Game
 {
     private readonly GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch = null;
+    private TextureRenderer _textureRenderer = null;
+    private InputManager _input = null;
+    private Camera2D _camera = null;
+    private Texture2D _pixel = null;
+    private Vector2 _position = new(100, 100);
+    public GameRoot()
+    {
+        _graphics = new GraphicsDeviceManager(this);
 
-    private SpriteBatch _spriteBact = null;
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
 
-    private TextureRenderer
+        Window.Title = "Aurora Engine";
+
+        _graphics.PreferredBackBufferWidth = 1280;
+        _graphics.PreferredBackBufferHeight = 720;
+
+        IsFixedTimeStep = true;
+        TargetElapsedTime = TimeSpan.FromSeconds(1d / 60d);
+    }
+
+
 
 }
