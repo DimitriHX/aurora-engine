cdf4549fa27225f748740966ff56f86eed4d2c8a
dimitrihx <dimitrihx@gmail.com>
2026-05-21 17:22:46 -0600

    Creamos la transition del personaje en Engine/Camera

---
+++ src/Aurora.Engine/Camera/Camera2D.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Aurora.Engine.Camera;
+
+public class Camera2D
+{
+    public Vector2 Position { get; set; }
+
+    public Matrix Transform => 
+        Matrix.CreateTranslation(
+            -Position.X,
+            -Position.Y,
+            0f
+        );
+}
