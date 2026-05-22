1a140d573b887744a0991e5a5792555bf624074d
dimitrihx <dimitrihx@gmail.com>
2026-05-21 19:57:07 -0600

    Agregue las escenas en engine para poder controlar los estados de manera independiente

---
+++ src/Aurora.Engine/Scenes/Scene.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Aurora.Engine.Scenes;
+
+public abstract class Scene
+{
+    public virtual void Initialize()
+    {
+    }
+
+    public virtual void Load()
+    {
+    }
+
+    public virtual void Unload()
+    {
+
+    }
+
+    public virtual void Update(GameTime gameTime)
+    {
+    }
+
+    public virtual void Draw(GameTime gameTime)
+    {
+    }
+}
---
+++ src/Aurora.Engine/Scenes/SceneManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Aurora.Engine.Scenes;
+
+public class SceneManager
+{
+    public Scene? ActiveScene { get; private set; }
+
+    public void ChangeScene(Scene newScene)
+    {
+        ActiveScene?.Unload();
+
+        ActiveScene = newScene;
+
+        ActiveScene.Initialize();
+        ActiveScene.Load();
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        ActiveScene?.Update(gameTime);
+    }
+
+    public void Draw(GameTime gameTime)
+    {
+        ActiveScene?.Draw(gameTime);
+    }
+}
