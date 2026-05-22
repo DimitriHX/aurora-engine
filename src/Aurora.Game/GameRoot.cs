2b674b024e9a6448e62cc34060c3539043f87bc8
dimitrihx <dimitrihx@gmail.com>
2026-05-21 18:01:01 -0600

    agregue la codificacion funcional de GameRoot usando las demas librerias

--- src/Aurora.Game/GameRoot.cs
+++ src/Aurora.Game/GameRoot.cs
@@ -32,6 +32,64 @@ public class GameRoot : Microsoft.Xna.Framework.Game
         TargetElapsedTime = TimeSpan.FromSeconds(1d / 60d);
     }
 
+    protected override void Initialize()
+    {
+        _input = new InputManager();
+        _camera = new Camera2D();
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        _textureRenderer = new TextureRenderer(_spriteBatch);
+
+        _pixel = new Texture2D(GraphicsDevice, 1, 1);
+
+        _pixel.SetData(new[] { Color.White });
+    }
 
 
+    protected override void Update(GameTime gameTime)
+    {
+        _input.Update();
+
+        if (_input.ExitRequested())
+            Exit();
+        float speed = 200f;
+        float delta = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        if (_input.Left())
+            _position.X -= speed * delta;
+
+        if (_input.Right())
+            _position.X += speed * delta;
+
+        if (_input.Up())
+            _position.Y -= speed * delta;
+
+        if (_input.Down())
+            _position.Y += speed * delta;
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(new Color(20, 20, 30));
+
+        _spriteBatch.Begin(transformMatrix: _camera.Transform);
+
+        _textureRenderer.Draw(
+            _pixel,
+            _position,
+            new Vector2(32,32),
+            Color.CornflowerBlue
+        );
+
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
 }
