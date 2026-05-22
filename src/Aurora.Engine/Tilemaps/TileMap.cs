b7dccc80dff40b767fc24f36dc7fd281b1639624
dimitrihx <dimitrihx@gmail.com>
2026-05-21 21:00:30 -0600

    Agregue la carpeta TileMaps y los archivos de los tilesets menos la logica del Tileset.cs

---
+++ src/Aurora.Engine/Tilemaps/TileMap.cs
@@ -0,0 +1,29 @@
+﻿namespace Aurora.Engine.Tilemaps;
+
+public class TileMap
+{
+    public int Width { get; }
+    public int Height { get; }
+    public int TileSize { get; }
+    public int[] Tiles { get; }
+    public TileMap(
+        int width,
+        int height,
+        int tileSize
+        )
+    {
+        Width = width;
+        Height = height;
+        TileSize = tileSize;
+    }
+
+    public int GetTile(int x, int y)
+    {
+        return Tiles[y * Width + x];
+    }
+
+    public void SetTile(int x, int y, int value)
+    {
+        Tiles[y * Width + x] = value;
+    }
+}
---
+++ src/Aurora.Engine/Tilemaps/TileRenderer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Aurora.Engine.Tilemaps;
+
+public class TileRenderer
+{
+    private readonly SpriteBatch _spriteBatch;
+
+    public TileRenderer(SpriteBatch spriteBatch)
+    {
+        _spriteBatch = spriteBatch;
+    }
+
+    public void Draw(
+        TileMap map,
+        Texture2D texture
+        )
+    {
+        for (int y = 0; y < map.Height; y++)
+        {
+            for (int x = 0; x < map.Width; x++)
+            {
+                int tile = map.GetTile(x, y);
+
+                Color color = tile switch
+                {
+                    0 => new Color(50, 120, 50),
+                    1 => new Color(30, 30, 160),
+                    _ => Color.Magenta
+                };
+
+                Rectangle rectangle = new(
+                    x * map.TileSize,
+                    y * map.TileSize,
+                    map.TileSize,
+                    map.TileSize
+                );
+
+                _spriteBatch.Draw(
+                    texture,
+                    rectangle,
+                    color
+                );
+            }
+        }
+    }
+}
