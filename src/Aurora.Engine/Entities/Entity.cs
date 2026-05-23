7dee022fc630e5d2d4f0045465c9a1ee52358bb8
dimitrihx <dimitrihx@gmail.com>
2026-05-22 19:51:05 -0600

    Agregue el manejo de entidades y separe la entidad de render para mayor escalabilidad

---
+++ src/Aurora.Engine/Entities/Entity.cs
@@ -0,0 +1,6 @@
+﻿namespace Aurora.Engine.Entities;
+
+public class Entity
+{
+    public Transform Transform { get; } = new();
+}
---
+++ src/Aurora.Engine/Rendering/EntityRenderer.cs
@@ -0,0 +1,35 @@
+﻿using Aurora.Engine.Entities;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Aurora.Engine.Rendering;
+public class EntityRenderer
+{
+    private readonly SpriteBatch _spriteBatch;
+
+    public EntityRenderer(SpriteBatch spriteBatch)
+    {
+        _spriteBatch = spriteBatch;
+    }
+
+    public void Draw(
+        SpriteEntity entity,
+        Vector2 size
+        )
+    {
+        Rectangle destination = new(
+            (int)entity.Transform.Position.X,
+            (int)entity.Transform.Position.Y,
+            (int)size.X,
+            (int)size.Y
+        );
+
+        _spriteBatch.Draw(
+            entity.Texture,
+            destination,
+            Color.White
+        );
+    }
+}
